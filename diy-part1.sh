4f41f03a8bf95be045689b73bd9a2b4606cb401f
pencail1 <123568721+pencail1@users.noreply.github.com>
2023-02-03 18:02:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,5 +20,7 @@
 
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
+
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/XXKDB/luci-theme-argon_armygreen.git package/luci-theme-argon_armygreen
  
